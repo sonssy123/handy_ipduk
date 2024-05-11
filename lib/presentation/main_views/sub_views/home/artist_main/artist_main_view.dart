762acd5b745e085a31a6a1b56ad3a8ffc1956685
Sung yong _ Alice <sonssy012@gmail.com>
2024-05-11 11:52:29 +0900

    artist main view 추가

--- lib/presentation/main_views/sub_views/home/artist_main/artist_main_view.dart
+++ lib/presentation/main_views/sub_views/home/artist_main/artist_main_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:handy_ipduk/main_screen/tab_screen/tab_screen_view.dart';
+import 'package:handy_ipduk/presentation/extenstions/color_extension.dart';
 import 'package:handy_ipduk/presentation/loading_bar/loading_bar_animation.dart';
 import 'package:handy_ipduk/presentation/main_views/sub_views/home/image/artist_main_view_image.dart';
 import 'package:handy_ipduk/presentation/main_views/views/calendar_view.dart';
 import 'package:handy_ipduk/presentation/main_views/sub_views/home/artist_profile/artist_profile_view.dart';
-import 'package:handy_ipduk/presentation/main_views/views/shop_view.dart';
+import 'package:handy_ipduk/presentation/utils/size_converter.dart';
 
 class ArtistMainView extends StatefulWidget {
-  const ArtistMainView({super.key});
+  const ArtistMainView({Key? key}) : super(key: key);
 
   @override
   State<ArtistMainView> createState() => _ArtistMainViewState();
@@ -14,6 +16,19 @@ class ArtistMainView extends StatefulWidget {
 
 class _ArtistMainViewState extends State<ArtistMainView> {
   bool _isLoading = false;
+  String _selectedTab = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedTab = 'Feed';
+  }
+
+  void _selectTab(String tabName) {
+    setState(() {
+      _selectedTab = tabName;
+    });
+  }
 
   void _navigateToArtistProfile() {
     setState(() {
@@ -32,11 +47,31 @@ class _ArtistMainViewState extends State<ArtistMainView> {
     });
   }
 
+  Widget _buildTab(String tabName, String text, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+              color: _selectedTab == tabName
+                  ? ColorExtension.accentColor
+                  : Colors.black,
+            ),
+          ),
+          if (_selectedTab == tabName)
+            Container(width: 30, height: 3, color: ColorExtension.accentColor),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.amber, // 배경
         appBar: AppBar(
           title: const Text('Artist Main'),
           centerTitle: true,
@@ -55,10 +90,11 @@ class _ArtistMainViewState extends State<ArtistMainView> {
           children: [
             SingleChildScrollView(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
                     width: double.infinity,
-                    height: 500,
+                    height: 550, // 사이즈
                     decoration: BoxDecoration(
                       image: DecorationImage(
                         image: NetworkImage(
@@ -73,9 +109,9 @@ class _ArtistMainViewState extends State<ArtistMainView> {
                         GestureDetector(
                           onTap: _navigateToArtistProfile,
                           child: Padding(
-                            padding: const EdgeInsets.only(bottom: 10.0),
+                            padding: const EdgeInsets.only(bottom: 20.0), // 가수
                             child: GestureDetector(
-                              child: Row(
+                              child: const Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   Text(
@@ -96,9 +132,86 @@ class _ArtistMainViewState extends State<ArtistMainView> {
                             ),
                           ),
                         ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.symmetric(vertical: 10.0), // 탭
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              _buildTab('Feed', 'Feed', () {
+                                _selectTab('Feed');
+                              }),
+                              _buildTab('Artist', 'Artist', () {
+                                _selectTab('Artist');
+                              }),
+                              _buildTab('Schedule', 'Schedule', () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const CalendarView(),
+                                  ),
+                                );
+                              }),
+                              _buildTab('Shop', 'Shop', () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const MainTabScreenView(),
+                                  ),
+                                );
+                              }),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
+                  SizedBox(height: SizeConverter.getHeight(context, 8)), // 제목
+                  if (_selectedTab == 'Feed')
+                    const Padding(
+                      padding: EdgeInsets.only(left: 0.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            child: Text(
+                              'Post',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            child: Text(
+                              '팬 게시판',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  if (_selectedTab == 'Artist')
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            child: Text(
+                              '아티스트 게시판',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                 ],
               ),
             ),
@@ -109,57 +222,6 @@ class _ArtistMainViewState extends State<ArtistMainView> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class ArtistProfileButton extends StatelessWidget {
-  const ArtistProfileButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const ArtistProfileView()),
-        );
-      },
-      child: const Text('Artist Profile 화면으로 이동'),
-    );
-  }
-}
-
-class ShopButton extends StatelessWidget {
-  const ShopButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const ShopView()),
-        );
-      },
-      child: const Text('Shop 화면으로 이동'),
-    );
-  }
-}
-
-class CalendarButton extends StatelessWidget {
-  const CalendarButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const CalendarView()),
-        );
-      },
-      child: const Text('Calendar 화면으로 이동'),
     );
   }
 }
